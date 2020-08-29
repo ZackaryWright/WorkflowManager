04633eb7b17ec9e85c38d68e05f9fb6ec14de23a
ZackaryWright <zwrig004@fiu.edu>
2020-08-29 11:18:34 -0400

    sound functionality and reset button

--- index.js
+++ index.js
@@ -1,21 +1,46 @@
 // Elements
 const timer = document.getElementById('timer');
-const btn = document.querySelector('.start-btn');
+const startBtn = document.querySelector('.start-btn');
+const pauseBtn = document.querySelector('.pause-btn');
+const resetBtn = document.querySelector('.reset-btn');
 
-var startingMinutes = 20;
+var alert = new Audio("sounds/alert.mp3");
+
+var startingMinutes = 1;
 let time = startingMinutes * 60;
+let intervalId = null;
+
+let pressed = 0;
 
 // Event Listeners
-btn.addEventListener('click', startTimer)
+startBtn.addEventListener('click', function() {
+  pauseBtn.style.display = 'inline-block';
+  resetBtn.style.visibility = 'visible';
+  intervalId = setInterval(startTimer, 1000);
+})
 
-setInterval(startTimer, 1000);
+pauseBtn.addEventListener('click', stopTimer);
+
+resetBtn.addEventListener('click', resetTimer);
 
 function startTimer () {
-  const minutes = Math.floor(time / 60);
-  let seconds = time % 60;
 
-  seconds = seconds < 10 ? '0' + seconds : seconds;
+      pressed = 1;
+      const minutes = Math.floor(time / 60);
+      let seconds = time % 60;
 
-  timer.innerHTML = minutes +":"+seconds;
-  time--;
+      seconds = seconds < 10 ? '0' + seconds : seconds;
+
+      timer.innerHTML = minutes +":"+seconds;
+      time !== 0 ? time-- : alert.play();
+}
+
+function stopTimer () {
+  clearInterval(intervalId);
+}
+
+function resetTimer() {
+  pressed = 0;
+  stopTimer();
+  timer.innerHTML = "20:00";
 }
