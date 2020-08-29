2e2d8ee439ff97c47c12a3364f8a9fd77adb41f6
ZackaryWright <zwrig004@fiu.edu>
2020-08-28 23:21:56 -0400

    update

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-function timer () {
-  var sec = 30;
-  var timer = d
+const var startingMinutes = 20;
+const timer = document.getElementById('clock');
+var time = startingMinutes * 60;
+
+setInterval(clockCountdown(), 1000);
+
+function clockCountdown() {
+  const minutes = Math.floor(time/60);
+  const seconds = time % 60;
+
+  if(seconds < 10) {
+    seconds = '0' + seconds;
+  }
+
+  timer.innerHTML = minutes + ":" + seconds;
+
+  time--;
 }
