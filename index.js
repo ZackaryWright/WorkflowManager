9960d20bfc143f3cf95fea8b4f9b5ec8c5f75c1e
ZackaryWright <zwrig004@fiu.edu>
2020-08-30 09:55:06 -0400

    debugging

--- index.js
+++ index.js
@@ -3,46 +3,44 @@ const timer = document.getElementById('timer');
 const startBtn = document.querySelector('.start-btn');
 const pauseBtn = document.querySelector('.pause-btn');
 const resetBtn = document.querySelector('.reset-btn');
-
-var alert = new Audio("sounds/alert.mp3");
-
-var startingMinutes = 25;
+let startingMinutes = 25;
 let time = startingMinutes * 60;
 let intervalId = null;
-
-let pressed = 0;
+const alert = new Audio("sounds/alert.mp3");
 
 // Event Listeners
-startBtn.addEventListener('click', function() {
-  resetTimer();
-  pauseBtn.style.display = 'inline-block';
-  resetBtn.style.visibility = 'visible';
-  intervalId = setInterval(startTimer, 1000);
-})
+startBtn.addEventListener('click', visibleStart);
 
 pauseBtn.addEventListener('click', stopTimer);
 
 resetBtn.addEventListener('click', resetTimer);
 
-function startTimer () {
+function visibleStart () {
+  pauseBtn.style.display = 'inline-block';
+  resetBtn.style.visibility = 'visible';
+  intervalId = setInterval(startTimer, 1000);
+}
 
-      pressed = 1;
+// Starts Countdown
+function startTimer () {
+      startBtn.removeEventListener('click', visibleStart);
       const minutes = Math.floor(time / 60);
       let seconds = time % 60;
-
       seconds = seconds < 10 ? '0' + seconds : seconds;
-
       timer.innerHTML = minutes +":"+seconds;
       time !== 0 ? time-- : alert.play();
 }
 
+// Pauses Countdown
 function stopTimer () {
   clearInterval(intervalId);
+  startBtn.addEventListener('click', visibleStart);
 }
 
+// Clears clock and starts countdown over
 function resetTimer() {
-  pressed = 0;
   stopTimer();
   time = 1500;
   timer.innerHTML = "25:00";
+  startBtn.addEventListener('click', visibleStart);
 }
