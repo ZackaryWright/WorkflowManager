84f234af9d905bf69cd6728c806829c41b68766d
ZackaryWright <zwrig004@fiu.edu>
2020-09-03 10:21:13 -0400

    update

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 // Elements
 const timer = document.getElementById('main-timer');
+const breakTimer = document.getElementById('break-timer');
 const startBtn = document.querySelector('.start-btn');
 const pauseBtn = document.querySelector('.pause-btn');
 const resetBtn = document.querySelector('.reset-btn');
-let startingMinutes = 25;
+const breakContainer = document.querySelector('.break-container');
+const breakBtn = document.querySelector('.break-btn');
+const breakPauseBtn = document.querySelector('break-pause-btn');
+let startingMinutes = 1;
+let breakStartMin = 5;
 let time = startingMinutes * 60;
+let breakTime = breakStartMin * 60;
 let intervalId = null;
+let intervalId2 = null;
 const alert = new Audio("sounds/alert.mp3");
 
 // Event Listeners
 startBtn.addEventListener('click', visibleStart);
 pauseBtn.addEventListener('click', stopTimer);
 resetBtn.addEventListener('click', resetTimer);
-
+breakBtn.addEventListener('click', )
 
 // Makes various elements visible and sets timer interval
 function visibleStart () {
@@ -28,7 +35,9 @@ function startTimer () {
       let seconds = time % 60;
       seconds = seconds < 10 ? '0' + seconds : seconds;
       timer.innerHTML = minutes +":"+seconds;
-      time !== 0 ? time-- : alert.play() + alert('5-Minute Break Timer will start (Press Ok)');
+      time !== 0 ? time-- : alert.play()
+      time === 0 ? breakContainer.style.visibility = 'visible' :
+      breakContainer.style.visibility = 'hidden';
 }
 
 // Pauses Countdown
@@ -43,4 +52,17 @@ function resetTimer() {
   time = 1500;
   timer.innerHTML = "25:00";
   startBtn.addEventListener('click', visibleStart);
+}
+
+function breakBtnsVisible() {
+  breakPauseBtn.style.visibility = 'visible';
+  intervalId2 = setInterval(startBreak, 1000);
+}
+
+function startBreak() {
+  breakBtn.removeEventListener('click', breakBtnsVisible);
+  const minutes = Math.floor(breakTime / 60);
+  let seconds = breakTime % 60;
+  seconds = seconds < 10 ? '0' + seconds : seconds;
+  timer.innerHTML = minutes +":"+seconds;
 }
